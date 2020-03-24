d25588177ca26b2ffecd32b16566643b5f3977d4
David Jaquet <djaquet5@gmail.com>
2020-03-24 10:14:06 +0100

    Create the head, body and nose

---
+++ Snowman.py
@@ -0,0 +1,59 @@
+import vtk
+import time
+
+# Head
+head = vtk.vtkSphereSource()
+head.SetCenter(0, 0, 0)
+head.SetRadius(8.0)
+head.SetThetaResolution(50)
+
+headMapper = vtk.vtkPolyDataMapper()
+headMapper.SetInputConnection(head.GetOutputPort())
+
+headActor = vtk.vtkActor()
+headActor.SetMapper(headMapper)
+
+# Body
+body = vtk.vtkSphereSource()
+body.SetCenter(20, 0, 0)
+body.SetRadius(10.0)
+body.SetThetaResolution(50)
+
+bodyMapper = vtk.vtkPolyDataMapper()
+bodyMapper.SetInputConnection(body.GetOutputPort())
+
+bodyActor = vtk.vtkActor()
+bodyActor.SetMapper(bodyMapper)
+
+# Nose
+nose = vtk.vtkConeSource()
+nose.SetCenter(40, 0, 0)
+nose.SetHeight(3.0)
+nose.SetRadius(1.0)
+nose.SetResolution(50)
+
+noseMapper = vtk.vtkPolyDataMapper()
+noseMapper.SetInputConnection(nose.GetOutputPort())
+
+noseActor = vtk.vtkActor()
+noseActor.SetMapper(noseMapper)
+noseActor.GetProperty().SetColor(0.925, 0.65, 0)
+
+# Renderer
+renderer = vtk.vtkRenderer()
+renderer.AddActor(headActor)
+renderer.AddActor(bodyActor)
+renderer.AddActor(noseActor)
+
+# Color picked from the demo video
+renderer.SetBackground(1, 0.894, 0.898)
+
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(renderer)
+renWin.SetSize(600, 600)
+
+for i in range(0, 360):
+    time.sleep(0.03)
+
+    renWin.Render()
+    renderer.GetActiveCamera().Azimuth(1)
